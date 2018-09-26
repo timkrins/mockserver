48c3015aea18f2fac25f1b1776db100cda39f51c
Tim Krins <timothy.krins@calligo.cloud>
2018-09-26 12:58:08 +0100

    Add SSL option

--- bin/mockserver.js
+++ bin/mockserver.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
 var http        = require('http');
+var https       = require('https');
 var mockserver  = require('./../mockserver');
 var argv        = require('yargs').argv;
 var colors      = require('colors')
+var fs          = require('fs')
 var info        = require('./../package.json');
 var mocks       = argv.m || argv.mocks;
 var port        = argv.p || argv.port;
 var verbose     = !(argv.q || argv.quiet);
+
+var ssl         = argv.ssl;
+var cert        = argv.cert;
+var key         = argv.key;
 
 if (!mocks || !port) {
   console.log([
@@ -20,17 +26,26 @@ if (!mocks || !port) {
     "  -p, --port=PORT    - Port to listen on",
     "  -m, --mocks=PATH   - Path to mock files",
     "  -q, --quiet        - Do not output anything",
+    "  --ssl --cert=[cert] --key=[key] - Use SSL",
     "",
     "Example:",
     "  mockserver -p 8080 -m './mocks'"
   ].join("\n"));
 } else {
-  http.createServer(mockserver(mocks, verbose)).listen(port);
+  if(ssl) {
+    var options = {
+      key: fs.readFileSync(key),
+      cert: fs.readFileSync(cert)
+    };
+    https.createServer(options, mockserver(mocks, verbose)).listen(port);
+  } else {
+    http.createServer(mockserver(mocks, verbose)).listen(port);
+  }
 
   if (verbose) {
     console.log('Mockserver serving mocks {'
       + 'verbose'.yellow + ':' + (verbose && 'true'.green || 'false')
       + '} under "' + mocks.green  + '" at '
-      + 'http://localhost:'.green + port.toString().green);
+      + ((ssl && 'https' || 'http') + '://localhost:' + port.toString()).green);
   }
 }
